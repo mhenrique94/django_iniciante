b3c5aafef1aa7dd4662d7c21f904adf7728b275b
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-29 22:01:13 -0300

    Atualizado settings.py

--- teste/settings.py
+++ teste/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 SECRET_KEY = 'django-insecure-&lar(*ptw(c6(wtsnhoov8h*d)1y%wi7(%xjc&)t%-7*on@!(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
